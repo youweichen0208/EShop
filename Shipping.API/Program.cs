98f82e4ca535f220967f9d84f5b734bf66cf2945
Youwei Chen <youweichen0208@gmail.com>
2024-03-18 00:57:29 -0400

    "updates"

---
+++ Shipping.API/Program.cs
@@ -0,0 +1,31 @@
+using ApplicationCore.Contracts;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ShippingDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("ShippingDB"));
+});
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
